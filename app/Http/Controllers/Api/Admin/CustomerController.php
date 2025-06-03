574724427bfb1aff59b2e3f10f0d3bb20557262c
trianandafajar <mail.trianandafajar@gmail.com>
2025-06-03 09:21:11 +0700

    Update Custommer Controller

--- app/Http/Controllers/Api/Admin/CustomerController.php
+++ app/Http/Controllers/Api/Admin/CustomerController.php
@@ -15,9 +15,9 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        $customers = Customer::when(request()->q, function($customers) {
-            $customers = $customers->where('name', 'like', '%'. request()->q . '%');
-         })->latest()->paginate(5);
+        $customers = Customer::when(request()->q, function ($customers) {
+            $customers = $customers->where('name', 'like', '%' . request()->q . '%');
+        })->latest()->paginate(5);
 
         //return with Api Resource
         return new CustomerResource(true, 'List Data Customer', $customers);
